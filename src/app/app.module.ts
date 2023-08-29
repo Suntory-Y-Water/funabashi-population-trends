f339a729570e129a01ea939641547e5302928d42
sui <1202ayasnppk@gmail.com>
2023-08-29 22:36:35 +0900

    tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'topbar', component: TopBarComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
