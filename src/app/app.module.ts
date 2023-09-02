f418a93a818b7fdbef82f7ee99d6e9add6c5574a
sui <1202ayasnppk@gmail.com>
2023-09-02 13:15:08 +0900

    update: チュートリアルの削除

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartComponent } from './cart/cart.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   
   declarations: [
     AppComponent,
-    ProductListComponent,
-    TopBarComponent,
-    ProductAlertsComponent,
-    ProductDetailsComponent,
-    CartComponent,
-    ShippingComponent
   ],
   imports: [
     BrowserModule,
@@ -29,11 +17,6 @@ import { ShippingComponent } from './shipping/shipping.component';
     ReactiveFormsModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'topbar', component: TopBarComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent },
-      { path: 'shipping', component: ShippingComponent },
     ]),
   ],
   providers: [],
