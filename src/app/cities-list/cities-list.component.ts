60a07bd4cf68de52dd1d99c6464d7c5c3076939c
sui <1202ayasnppk@gmail.com>
2023-09-03 09:12:49 +0900

    update: ボタン選択時の色を変更し、値を渡せるように変更

--- src/app/cities-list/cities-list.component.ts
+++ src/app/cities-list/cities-list.component.ts
@@ -7,5 +7,21 @@ import { cities } from '../population/cities';
   styleUrls: ['./cities-list.component.css']
 })
 export class CitiesListComponent {
-  cities = [...cities]
+  cities = cities;
+  selectedCityAreaNames: string[] = [];
+
+  ngOnInit() {
+  }
+
+  onButtonClick(areaName: string) {
+    if (this.selectedCityAreaNames.includes(areaName)) {
+      // すでに選択されている場合、配列から削除する
+      this.selectedCityAreaNames = this.selectedCityAreaNames.filter(name => name !== areaName);
+    } else {
+      // 新しく選択された場合、配列に追加する
+      this.selectedCityAreaNames.push(areaName);
+    }
+    console.log(`Current selected cities: ${this.selectedCityAreaNames}`);
+  }
+  
 }
