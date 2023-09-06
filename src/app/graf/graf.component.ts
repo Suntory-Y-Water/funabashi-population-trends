799b329df0a4a4d88b3f4a9ea85b1207817a01c8
sui <1202ayasnppk@gmail.com>
2023-09-07 07:37:18 +0900

    update: ボタン押下でグラフの表示を増減できる仕様に変更

--- src/app/graf/graf.component.ts
+++ src/app/graf/graf.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { POPULATION_CATEGORIES } from '../population/constants';
+import { POPULATION_CATEGORIES } from '../population/populationCategory';
+import { PopulationCategory, PopulationData } from './graf.types'
 import { SelectedDataService } from '../shared/shared.service';
 import { ChartConfiguration, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
@@ -7,36 +8,22 @@ import { cities } from '../population/cities';
 import { HttpClient } from '@angular/common/http';
 
 
-interface PopulationCategory {
-  id: number;
-  label: string;
-}
-interface PopulationData {
-  id: number;
-  area_id: number;
-  year: number;
-  total_population: number;
-  young_population: number;
-  working_age_population: number;
-  elderly_population: number;
-}
-
 @Component({
   selector: 'app-graf',
   templateUrl: './graf.component.html',
   styleUrls: ['./graf.component.css']
 })
+
 export class GrafComponent implements OnInit {
-  populationDataset: PopulationData[] = [];
   selectedCategory: PopulationCategory | null = null;
   populationCategories = POPULATION_CATEGORIES;
+  private populationDataset: PopulationData[] = [];
+  private populationDataUrl = '../../assets/population.json';
 
   constructor(
     private selectedDataService: SelectedDataService,
     private http: HttpClient
   ) {}
-
-  private populationDataUrl = '../../assets/population.json';
 
   loadPopulationData() {
     this.http.get<PopulationData[]>(this.populationDataUrl).subscribe(data => {
@@ -46,7 +33,6 @@ export class GrafComponent implements OnInit {
 
   filterPopulationDataByAreaId(areaId: number) {
     const filteredData = this.populationDataset.filter(data => data.area_id === areaId);
-   // グラフを更新するためのデータを取得
     const graphData = filteredData.map(data => {
       switch (this.selectedCategory?.label) {
         case "総人口":
@@ -64,16 +50,24 @@ export class GrafComponent implements OnInit {
     return graphData;
   }
   
+  
   ngOnInit() {
     this.loadPopulationData();
 
     this.selectedDataService.selectedCityAreaNames$.subscribe(cityAreaNames => {
-      const firstCityAreaName = cityAreaNames[0];
-      const matchingCity = cities.find(city => city.areaName === firstCityAreaName);
-      if (matchingCity) {
-        this.filterPopulationDataByAreaId(matchingCity.id);
-      }
-      console.log(`地域が更新されました${firstCityAreaName}`);
+      // 追加: 現在のdatasetsから削除すべき都市のデータセットを特定
+      const datasetsCityNames = this.lineChartData.datasets.map(dataset => dataset.label).filter((label): label is string => !!label);
+      const citiesToRemove = datasetsCityNames.filter(cityName => !cityAreaNames.includes(cityName as string));
+      // 削除すべき都市のデータセットを削除
+      citiesToRemove.forEach(cityName => this.removeCityData(cityName));
+
+      cityAreaNames.forEach(cityAreaName => {
+        const matchingCity = cities.find(city => city.areaName === cityAreaName);
+        if (matchingCity) {
+          this.addOrUpdateCityData(matchingCity.id, cityAreaName);
+        }
+      });
+      console.log(`地域が更新されました: ${cityAreaNames.join(', ')}`);
     });
     
     this.selectedDataService.selectedCategory$.subscribe(category => {
@@ -81,47 +75,66 @@ export class GrafComponent implements OnInit {
     });
   }
   
-  onCategoryChange() {
-    if (this.selectedCategory) {
-      this.selectedDataService.setSelectedCategory(this.selectedCategory.label);
-      this.updateGraphData();
+  addOrUpdateCityData(cityId: number, cityName: string) {
+    const graphData = this.filterPopulationDataByAreaId(cityId);
+
+    // 既存のデータセットを検索
+    const existingDataset = this.lineChartData.datasets.find(dataset => dataset.label === cityName);
+
+    if (existingDataset) {
+        existingDataset.data = graphData;
     } else {
-      this.selectedDataService.setSelectedCategory(null);
+        this.lineChartData.datasets.push({
+            data: graphData,
+            label: cityName,
+            // 他の設定 (色など) をここに追加
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        });
+    }
+
+    if (this.chart) {
+        this.chart.update();
     }
   }
-  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
-  updateGraphData() {
-    if (!this.selectedCategory) return;
-    const firstCityAreaName = this.selectedDataService.getSelectedCityAreaNames()[0];
-    const matchingCity = cities.find(city => city.areaName === firstCityAreaName);
-    if (matchingCity) {
-      const graphData = this.filterPopulationDataByAreaId(matchingCity.id);
-      this.lineChartData.datasets[0].data = graphData;
-      this.lineChartData.datasets[0].label = this.selectedCategory.label;
-      
-      console.log(`dataの値は${graphData}`);
-      console.log(`labelの値は${this.selectedCategory.label}`);
-      
+  removeCityData(cityName: string) {
+    const indexToRemove = this.lineChartData.datasets.findIndex(dataset => dataset.label === cityName);
+    if (indexToRemove !== -1) {
+      this.lineChartData.datasets.splice(indexToRemove, 1);
       if (this.chart) {
         this.chart.update();
       }
     }
   }
-  
+
+  onCategoryChange() {
+    if (this.selectedCategory) {
+      this.selectedDataService.setSelectedCategory(this.selectedCategory.label);
+      this.updateGraphDataForCategory();
+    } else {
+      this.selectedDataService.setSelectedCategory(null);
+    }
+  }
+
+  updateGraphDataForCategory() {
+    const cityAreaNames = this.selectedDataService.getSelectedCityAreaNames();
+    cityAreaNames.forEach(cityAreaName => {
+      const matchingCity = cities.find(city => city.areaName === cityAreaName);
+      if (matchingCity) {
+        this.addOrUpdateCityData(matchingCity.id, cityAreaName);
+      }
+    });
+  }
+
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+
   public lineChartType: ChartType = 'line';
   
   public lineChartData: ChartConfiguration['data'] = {
-    datasets: [
-      {
-        data: [],
-        label: '',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-      },
-    ],
-    labels: ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022'],  // ラベルを更新
+    datasets: [],  // 初期値を空の配列に変更
+    labels: ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022'],
   };
 
   public lineChartOptions: ChartConfiguration['options'] = {
